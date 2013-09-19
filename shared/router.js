7667d1cdda08637f1d5c0f2be7554ec450c7840c
Tenari the Great <djz24@case.edu>
2013-09-19 13:54:19 -0400

    start

--- shared/router.js
+++ shared/router.js
@@ -3,6 +3,12 @@ Router.configure({
 });
 Router.map(function(){
   this.route('layout', {path: '/'});
+  this.route('subject', {
+    path: '/subject/:id',
+    data: function(){ return this.params.id;}
+  });
+  this.route('topic', {path: '/topic/:id'});
+  this.route('lesson', {path: '/lesson/:id'});
   this.route('showResults', {
     path: '/results/:id',
     data: function(){ return Lessons.findOne(this.params.id); }
